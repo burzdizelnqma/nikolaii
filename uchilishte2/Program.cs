57a33484cc47a991d692e8bd97702542a8b00104
nikolaihristov <124766753+nikolaihristov@users.noreply.github.com>
2023-02-09 10:39:19 +0200

    Add files via upload

---
+++ uchilishte2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uchilishte2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("vuvedi kolko dni e rabotil v meseca");
+            double n = double.Parse(Console.ReadLine());
+            Console.Write("vuvedi pechalba za den");
+            double m = double.Parse(Console.ReadLine());
+            Console.Write("vuvedi kursa dolara sprqmo leva");
+            double kurs = double.Parse(Console.ReadLine());
+
+            double mesec = n * m;
+            double godina = (mesec * 12) + (mesec * 2.5);
+        }
+    }
+}
